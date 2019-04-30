f837b8cd4f76cfdcd247f2e9061f4837e5d1ad3c
shahana <shahanam134@gmail.com>
2019-04-30 13:52:51 +0530

    program1 commit

---
+++ five.bat
@@ -0,0 +1,3 @@
+set classpath=D:\Practicegit\Programs\lib\*;D:\Practicegit\Programs\bin
+java org.testng.TestNG testng.xml
+pause;
---
+++ src/programs/Program1.java
@@ -0,0 +1,13 @@
+package programs;
+
+import org.testng.annotations.Test;
+
+public class Program1
+{
+	@Test
+	public void q1()
+	{
+		System.out.println("hello java");
+	}
+
+}
